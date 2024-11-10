edcfaa4edc147fdcc48c63bd3395b2ff1045ad68
Igor <2dmitriy1d01@gmail.com>
2024-11-10 03:17:50 +0300

    Fixed naming

--- ml/app.py
+++ ml/app.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from pipes import find
+from pipelines import find
 from bot import bot
 
 
